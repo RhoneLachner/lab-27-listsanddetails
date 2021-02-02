0699ffc20ce6d1328d824c451cf1c08b2df83976
Rhone Lachner <fernandclay@gmail.com>
2021-02-02 13:41:58 -0800

    added xfiles API fetch call to allcharacters jsx

--- src/characters/AllCharacters.jsx
+++ src/characters/AllCharacters.jsx
@@ -1,8 +1,15 @@
+import { json } from 'express';
 import React, { Component } from 'react'
 
 export default class AllCharacters extends Component {
     state = {
         characters: []
+    }
+
+    componentDidMount() {
+        fetch('https://xfiles-api.herokuapp.com/api/v1/characters')
+        .then(res => res.json())
+        .then(res => this.setState({ characters: json.results }));
     }
 
     render() {
