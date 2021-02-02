f35921d742d0ced9670cbeeeb4b392a583d6d2a5
Rhone Lachner <fernandclay@gmail.com>
2021-02-02 14:33:25 -0800

    added CharacterList with character elements

---
+++ src/characters/CharacterList.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Character from './Character';
+
+const CharacterList = ({ characters }) => {
+    const characterElements = characters.map(character => (
+        <li key={character.id}>
+            <Character {...character} />
+        </li>
+
+    ));
+
+    return (
+        <ul>
+            {characterElements}
+        </ul>
+    );
+};
+
+CharacterList.PropTypes = {
+    characters: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired
+    })).isRequired
+};
+export default CharacterList
