7d0cd1b2af5b0dbbd2cf5ed50038e4430a8eb1c7
Rhone Lachner <fernandclay@gmail.com>
2021-02-02 14:34:10 -0800

    fixed typo

--- src/characters/CharacterList.jsx
+++ src/characters/CharacterList.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Character from './Character';
 
@@ -17,7 +17,7 @@ const CharacterList = ({ characters }) => {
     );
 };
 
-CharacterList.PropTypes = {
+CharacterList.propTypes = {
     characters: PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired,
